9a9fe64ff178be755a454ab8de993152a0a601d4
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-06-07 14:55:07 -0300

    Upgrade code

--- projeto-inicial/petshop/src/App.jsx
+++ projeto-inicial/petshop/src/App.jsx
@@ -3,16 +3,22 @@ import "./assets/css/base/base.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./paginas/Home";
 import Sobre from "./paginas/Sobre";
+import Pagina404 from './paginas/Pagina404'
+import Cabecalho from './components/Cabecalho'
 
 function App() {
   return (
     <BrowserRouter>
+      <Cabecalho />
       <Switch>
         <Route exact path="/">
           <Home />
         </Route>
         <Route path="/sobre">
           <Sobre />
+        </Route>
+        <Route>
+          <Pagina404 />
         </Route>
       </Switch>
     </BrowserRouter>
